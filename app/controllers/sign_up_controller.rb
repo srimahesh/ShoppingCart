2eff45285a64608efed8bcf4d9e8e04969e7a1d6
Mahesh Vutukuri <vutukurim@gmail.com>
2014-03-11 23:44:42 -0500

    Initial commit

---
+++ app/controllers/sign_up_controller.rb
@@ -0,0 +1,16 @@
+class SignUpController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoppingCart::Application.config.secret_key_base = '09d6f73bf40d351afaf8f30b777fbe8f953498c7f50483eb9a71f97830a5ea671dc51437f66095ca412fa53dfdcc415efe5c84a72807f67c99e3cef4a3d5cfe4'
